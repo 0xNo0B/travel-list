2dcaa669a964090c31dd90da4b938b98918785df
rakan <neuer1bay@gmail.com>
2023-07-09 13:50:15 +0300

    add sort, clear, intractive message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ const initialItems = [
 
 export default function App() {
   const [items, setItems] = useState([]);
-
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
