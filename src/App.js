d65d8e27cc008c47e1cc77852c2124cb20765130
rakan <neuer1bay@gmail.com>
2023-07-09 13:50:37 +0300

    add clear, sortedby, intractive message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import { useState } from "react";
 
 const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "chrger", quantity: 1, packed: false },
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "chrger", quantity: 1, packed: false },
 ];
 
 export default function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(initialItems);
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
 
+  function handleClearItems() {
+    const ask = window.confirm("Do you Want to Clear all the list? ❌❌");
+    if (!ask) return;
+    setItems((item) => []);
+  }
+
+  function handleDeleteItem(id) {
+    console.log(id);
+    setItems((items) => items.filter((items) => items.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((item) =>
+      item.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
-      <Stats />
+      <PackingList
+        items={items}
+        onDeletItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClear={handleClearItems}
+      />
+      <Stats numberOfItems={items} />
     </div>
   );
 }
@@ -62,33 +85,81 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeletItem, onToggleItem, onClear }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "descrition")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul className="list">
-        {items.map((item) => (
-          <Item item={item} key={item.id} />
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            onDeletItem={onDeletItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input"> Sort by inpu order</option>
+          <option value="descrition">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClear}>Clear lis</button>
+      </div>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeletItem, onToggleItem }) {
   return (
     <li>
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <span
+        style={
+          item.packed ? { textDecoration: "line-through", color: "silver" } : {}
+        }
+      >
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeletItem(item.id)}>❌</button>
     </li>
   );
 }
 
-function Stats() {
+function Stats({ numberOfItems }) {
+  if (!numberOfItems.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to you packing list🔥</em>
+      </p>
+    );
+  const numItem = numberOfItems.length;
+  const numPacked = numberOfItems.filter((item) => item.packed).length;
+  const precentage = Math.round((numPacked / numItem) * 100);
+
   return (
     <footer className="stats">
-      <em> 💼You have X items on your list, and you already packed X (X%) </em>
+      <em>
+        {" "}
+        {precentage === 100
+          ? "You got everything! ready to go ✈️"
+          : `💼You have ${numItem} items on your list, and you already packed ${numPacked} (${precentage}%)${" "}`}{" "}
+      </em>
     </footer>
   );
 }
